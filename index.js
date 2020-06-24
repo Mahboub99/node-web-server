a8e24a23ac102ed64b7ecd41ec6784f0c584eee7
Ahmed Mahboub <ahmed.mahboub99@gmail.com>
2020-06-24 13:34:55 +0200

    add onsubmit function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhttp = new XMLHttpRequest();
-xhttp.open("POST", "http://localhost:3000/", true);
-xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xhttp.send("fname=Henry&lname=Ford");
+function submitForm(event) {
+  event.preventDefault();
+  let fname = document.getElementById("fname").value;
+  let lname = document.getElementById("lname").value;
+  let age = document.getElementById("age").value;
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "http://localhost:3000/", false);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("fname=" + fname + "&lname=" + lname + "&age=" + age);
+}
